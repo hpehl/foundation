3ff91761693fcefd98c1d98bf3be083049ed417b
Harald Pehl <harald.pehl@gmail.com>
2024-06-07 11:46:27 +0200

    Fix GH workflows

--- model/src/main/java/org/jboss/hal/model/deployment/Deployments.java
+++ model/src/main/java/org/jboss/hal/model/deployment/Deployments.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 2024 Red Hat
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.jboss.hal.model.deployment;
 
 import java.util.List;
@@ -8,7 +23,6 @@ import jakarta.inject.Inject;
 import org.jboss.hal.dmr.Operation;
 import org.jboss.hal.dmr.ResourceAddress;
 import org.jboss.hal.dmr.dispatch.Dispatcher;
-import org.jboss.hal.env.Environment;
 import org.jboss.hal.model.server.Server;
 
 import elemental2.promise.Promise;
